baeea308a7fb4cfb3293d98b659e0cb81566ee89
Valeriy Semenenko <gearmobile@gmail.com>
2024-12-23 21:35:42 +0300

    next step

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,45 +1,44 @@
 <template>
-    <div class="home">
-        <button
-          class="btn"
-          @click="decrement"
-        >-</button
-        >
-        <span class="counter">{{ count }}</span>
-        <button
-          class="btn"
-          @click="increment"
-        >+</button
-        >
-    </div>
+  <div class="home">
+    <button
+      class="btn"
+      @click="decrement"
+    >-</button
+    >
+    <span class="counter">{{ count }}</span>
+    <button
+      class="btn"
+      @click="increment"
+    >+</button
+    >
+  </div>
 </template>
 
-
 <script setup>
-    import { ref } from 'vue';
-    
-    const count = ref(0);
-    const increment = () => count.value++;
-    const decrement = () => count.value--;
+import { ref } from 'vue';
+
+const count = ref(0);
+const increment = () => count.value++;
+const decrement = () => count.value--;
 </script>
 
 <style scoped lang="scss">
-    .home {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        gap: 20px;
-    }
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+}
 
-    .btn {
-        width: 50px;
-        height: 50px;
-        border-radius: 50%;
-        border: 1px solid #ccc;
-        cursor: pointer;
-    }
+.btn {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: 1px solid #ccc;
+  cursor: pointer;
+}
 
-    .counter {
-        font-size: 30px;
-    }
+.counter {
+  font-size: 30px;
+}
 </style>
